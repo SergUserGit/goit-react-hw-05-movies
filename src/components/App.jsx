5b7c355e5ff7f3ff71b840671db49864f234d9fa
Sergii Podriezienko <podreenko1986@gmail.com>
2023-03-20 13:55:57 +0200

    new changes
    new changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import Home from './Home/Home';
+import { Routes, Route } from 'react-router-dom';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +10,12 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/components/Home/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <section>
+      <h1>Trending today</h1>
+    </section>
+  );
+};
+
+export default Home;
