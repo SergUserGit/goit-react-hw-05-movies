d7d864d774d4f0e57f5c7c7d0388f0103d4b0676
Sergii Podriezienko <podreenko1986@gmail.com>
2023-03-21 12:41:00 +0200

    new date
    new date

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,12 +1,51 @@
 //import { useParams } from 'react-router-dom';
 
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+//545611;
+
+//fetch(
+//  'https://api.themoviedb.org/3/movie/545611?api_key=8bdb4b862ffa773481adb9dc6c5538df'
+//)
+// .then(response => {
+//  if (response.ok) {
+//    return response.json();
+//  }
+//  return Promise.reject(new Error('Немає даних по запиту'));
+// })
+// .then(data => {
+//    console.log(data);
+// })
+// .catch(error => {})
+// .finally(() => {});
 
 const MovieDetails = () => {
+  const [detailArray, setDetailArray] = useState({});
   const { movieId } = useParams();
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=8bdb4b862ffa773481adb9dc6c5538df`
+    )
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(new Error('Немає даних по запиту'));
+      })
+      .then(data => {
+        setDetailArray({ ...data });
+      })
+      .catch(error => {})
+      .finally(() => {});
+  }, [movieId]);
+
+  console.log(detailArray);
   return (
     <>
-      <h2>Это детальная информация про фильм, по id - {movieId}</h2>
+      <h2>{detailArray.title}</h2>
+      <p>User Score</p>
     </>
   );
 };
